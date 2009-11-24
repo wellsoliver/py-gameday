763568804a76b2c0188b2ed65bb57919063baf51
Wells Oliver <wells@submute.net>
2009-11-24 13:10:21 -0600

    object-ified

--- gameday.py
+++ gameday.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 import threading
 import xml.dom.minidom
+import libxml2
 import urllib
-import BeautifulSoup
+from BeautifulSoup import BeautifulSoup
 import MySQLdb
 import time
+import re
 
 TYPE = 'mlb'
 YEAR = 2009
@@ -12,35 +14,104 @@ BASE = 'http://gd2.mlb.com/components/game/%s/' % TYPE
 CNF='/home/wells/.my.cnf'
 DATABASE='gameday'
 
+class Game:
+	FIELDS = ['game_id', 'game_pk', 'home_sport_code', 'home_team_code', 'home_id', 'home_fname', 'home_sname', 'home_wins', \
+		'home_loss', 'away_team_code', 'away_id', 'away_fname', 'away_sname', 'away_wins', 'away_loss', 'status_ind', 'date']
+
+	def _parseBox(self, elem):
+		for key in elem.attributes.keys():
+			if key in Game.FIELDS:
+				val = elem.attributes[key].value
+
+				if key == 'date':
+					val = MySQLdb.DateFromTicks(time.mktime(time.strptime(val, '%B %d, %Y')))
+				elif val.isdigit():
+					val = int(val)
+				else:
+					val = str(val)
+				
+				setattr(self, key, val)
+
+	def save(self):
+		if self.status_ind == 'F':
+			sql = 'INSERT INTO game (%s) VALUES(%s)' % (','.join(Game.FIELDS), ','.join(['%s'] * len(Game.FIELDS)))
+			cursor.execute(sql, [getattr(self, field) for field in Game.FIELDS])
+			db.commit()
+			
+			for inning in self.innings:
+				for atbat in inning:
+					sql = 'INSERT INTO atbat (%s) VALUES(%s)' % (','.join(atbat.keys()), ','.join(['%s'] * len(atbat.keys())))
+					cursor.execute(sql, atbat.values())
+				db.commit()
+					
+
+	def __init__(self, href):
+		self.game_id = href.rstrip('/')
+		self.innings = []
+		
+		_info = self.game_id.split('_')
+		url = '%syear_%4d/month_%02d/day_%02d/%s/' % (BASE, int(_info[1]), int(_info[2]), int(_info[3]), self.game_id)
+		soup = BeautifulSoup(fetch(url))
+
+		box_url = '%sboxscore.xml' % url
+		contents = fetch(box_url)
+		
+		if contents is not None:
+			doc = xml.dom.minidom.parseString(contents)
+			if doc.getElementsByTagName('boxscore').length == 1:
+				self._parseBox(doc.getElementsByTagName('boxscore').item(0))
+		
+				innings_url = '%sinning/' % url
+				soup = BeautifulSoup(fetch(innings_url))
+		
+				inning = 0
+				for inning_link in soup.findAll('a'):
+					if re.search(r'inning_\d+\.xml', inning_link['href']):
+						inning_url = '%s%s' % (innings_url, inning_link['href'])
+						doc = xml.dom.minidom.parseString(fetch(inning_url))
+						self.innings.append([])
+				
+						values = {}
+						for atbat in doc.getElementsByTagName('atbat'):
+							half = atbat.parentNode.nodeName
+							for key in atbat.attributes.keys():
+								values[str(key)] = atbat.attributes[key].value
+							values['half'] = half
+							values['game_id'] = self.game_id
+							values['inning'] = inning + 1
+					
+							self.innings[inning].append(values)
+						inning += 1	
+
+				self.save()
+
+def fetch(url):
+	for i in xrange(10):
+		#print 'fetching %s (%d)' % (url, i)
+		try:
+			page = urllib.urlopen(url)
+		except IOError:
+			time.sleep(1)
+			continue
+
+		if page.getcode() == 404:
+			return None
+		else:
+			return page.read()
+		break
+
 db = MySQLdb.connect(host="localhost", read_default_file=CNF, db=DATABASE)
 cursor = db.cursor()
 url = '%syear_%4d/month_%02d/' % (BASE, YEAR, 5)
-soup = BeautifulSoup.BeautifulSoup(urllib.urlopen(url).read())
+soup = BeautifulSoup(fetch(url))
 
 for link in soup.findAll('a'):
 	if link['href'].find('day') >= 0:
 		day_url = '%s%s' % (url, link['href'])
-		day_soup = BeautifulSoup.BeautifulSoup(urllib.urlopen(day_url).read())
+		day_soup = BeautifulSoup(fetch(day_url))
 
 		for game_link in day_soup.findAll('a'):
 			if game_link['href'].find('gid_') >= 0:
-				game_url = '%s%s' % (day_url, game_link['href'])
-				game_soup = BeautifulSoup.BeautifulSoup(urllib.urlopen(game_url).read())
-				
-				linescore = '%slinescore.xml' % game_url
-				boxscore_url = '%sboxscore.xml' % game_url
+				game = Game(game_link['href'])
 
-				box = xml.dom.minidom.parseString(urllib.urlopen(boxscore_url).read()).getElementsByTagName('boxscore').item(0)
-
-				game_id = box.attributes['game_id'].value
-				home = box.attributes['home_fname'].value;
-				away = box.attributes['away_fname'].value;
-				date = time.strptime(box.attributes['date'].value, '%B %d %y')
-
-				print game_id, home, away, date
-				sql = 'INSERT INTO game (game_id, home, away, date) VALUES(%s)' % (','.join(['%s'] * 4))
-				cursor.execute(sql, [game_id, home, away, date])
-				db.commit()
-				db.close()
-				
-				raise SystemExit
+db.close()
--- gameday.sql
+++ gameday.sql
@@ -1,11 +1,42 @@
 DROP TABLE IF EXISTS `game`;
 CREATE TABLE `game` (
-        game_id varchar(30) default null,
+        game_id varchar(30) not null primary key,
 		game_pk int default null,
+
 		home_sport_code varchar(10) default null,
-		away_team_code varchar(5) default null,
-		home_team_code varchar(5) default null,
-        home varchar(30) default null,
-        away varchar(30) default null,
+		home_id int default null,
+		home_team_code varchar(3) default null,
+		home_fname varchar(30) default null,
+		home_sname varchar(30) default null,
+		home_wins int default null,
+		home_loss int default null,
+
+		away_id int default null,
+		away_team_code varchar(3) default null,
+		away_fname varchar(30) default null,
+		away_sname varchar(30) default null,
+		away_wins int default null,
+		away_loss int default null,
+		status_ind char(1) default null,
         date date default null
+) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS `atbat`;
+CREATE TABLE `atbat` (
+	game_id varchar(30) not null,
+	num int,
+	half varchar(10) default null,
+	b int default null,
+	s int default null,
+	o int default null,
+	score char(1) default null,
+	batter int default null,
+	stand char(1) default null,
+	b_height varchar(5) default null,
+	pitcher int default null,
+	p_throws char(1) default null,
+	des varchar(500) default null,
+	event varchar(200) default null,
+	home_team_runs int default 0,
+	away_team_runs int default 0
 ) ENGINE=InnoDB;
