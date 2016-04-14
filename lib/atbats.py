b0a62ca16a56d9d15205434844abeaf02a3d9e3f
Dave Stern <dave@davestern.com>
2016-04-14 11:07:08 -0400

    atbats.py: Added logic to track pitcher credited with runner

--- lib/atbats.py
+++ lib/atbats.py
@@ -87,7 +87,8 @@ class AtBats(list):
                 doc = minidom.parseString(Fetcher.fetch(inning_url))
 
                 inning = []
-
+                # keep track of pitchers by runner on base for the inning
+                pitchers_credited = {}
                 for atbat in doc.getElementsByTagName('atbat'):
                     values = {}
                     half = atbat.parentNode.nodeName
@@ -99,6 +100,9 @@ class AtBats(list):
                     values['inning'] = inning_num
                     values['pitches'] = []
                     values['runners'] = []
+
+                    batter = atbat.attributes['batter'].value
+                    pitcher = atbat.attributes['pitcher'].value
 
                     balls = 0
                     strikes = 0
@@ -116,27 +120,61 @@ class AtBats(list):
                         elif pitch.attributes['type'].value == 'S':
                             strikes = strikes + 1
 
-                    for runner in atbat.getElementsByTagName('runner'):
-                        runner_attr = {
-                            'game_id': game_id,
-                            'atbat': atbat.attributes['num'].value,
-                            'runner': runner.attributes['id'].value,
-                            'start_base': runner.attributes['start'].value,
-                            'end_base': runner.attributes['end'].value,
-                        }
-
-                        for attr in ('score', 'rbi', 'earned'):
-                            try:
-                                if runner.attributes[attr].value == 'T':
-                                    runner_attr[attr] = 1
-                            except:
-                                pass
-
-                        r = Runner(runner, **runner_attr)
-                        values['runners'].append(r)
-
+                    values, pitchers_credited = self._parse_runners(atbat, values, pitchers_credited)
 
                     inning.append(values)
 
                 self.append(inning)
                 inning_num += 1
+
+
+    def _parse_runners(self, atbat, values, pitchers_credited):
+        for runner in atbat.getElementsByTagName('runner'):
+            runner_attr = {
+                'game_id': values['game_id'],
+                'atbat': atbat.attributes['num'].value,
+                'runner': runner.attributes['id'].value,
+                'start_base': runner.attributes['start'].value,
+                'end_base': runner.attributes['end'].value,
+                'event': runner.attributes['event'].value,
+                # Default pitcher is the one for this atbat
+                'pitcher_credited': values['pitcher']
+            }
+            runner_id = runner.attributes['id'].value
+
+            if runner_id == values['batter']:
+                # The runner was the batter, so keep track of the pitcher
+                # who put him on base
+                pitchers_credited[runner_id] = values['pitcher']
+            else:
+                # In all other cases, use the original pitcher for this batter.
+                #
+                # TODO:
+                # If a pinch runner was added this will not be set correctly.
+                # Pinch runners will not be credited to the original
+                # pitcher because the <action> tag for offensive substitutions
+                # doesn't specify the player leaving without parsing the
+                # `des` attribute and we don't parse the <action/> tag now.
+                # Workaround is to default to the pitcher for the atbat
+                runner_attr['pitcher_credited'] = pitchers_credited.get(runner_id, values['pitcher'])
+
+            if runner.attributes['end'].value == '':
+                # The runner scored or was put out, so remove him
+                # because it is possible to be on base twice in an inning
+                try:
+                    del pitchers_credited[runner_id]
+                except:
+                    pass
+
+
+            for attr in ('score', 'rbi', 'earned'):
+                try:
+                    if runner.attributes[attr].value == 'T':
+                        runner_attr[attr] = 1
+                except:
+                    pass
+
+            r = Runner(runner, **runner_attr)
+            values['runners'].append(r)
+
+        return values, pitchers_credited
