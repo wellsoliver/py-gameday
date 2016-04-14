4a1a2382dfde2335476a15af04078f2acbe6af42
Dave Stern <dave@davestern.com>
2016-04-14 01:26:15 -0400

    atbats.py: Add runners to the database.
    This allows for calculation of runs, stolen bases, RBI and
    earned runs.

--- lib/atbats.py
+++ lib/atbats.py
@@ -13,7 +13,7 @@ class Pitch:
         values['batter'] = kwargs['batter'] if 'batter' in kwargs else None
         values['b'] = count['balls']
         values['s'] = count['strikes']
-        
+
         # these change a lot :(
         # tired of taking them from the XML element
         # because maybe I don't have them in the schema
@@ -25,7 +25,7 @@ class Pitch:
         for key in element.attributes.keys():
             if key in FIELDS:
                 values[key] = element.attributes[key].value
-        
+
         self.values = values
 
     def save(self):
@@ -35,32 +35,49 @@ class Pitch:
         DB.query(sql, self.values.values())
         DB.save()
 
+
+class Runner:
+    def __init__(self, element, **kwargs):
+        self.values = kwargs
+
+    def save(self):
+        DB = store.Store()
+
+        sql = 'REPLACE INTO runner (%s) VALUES(%s)' % (','.join(self.values.keys()), ','.join(['%s'] * len(self.values)))
+        DB.query(sql, self.values.values())
+        DB.save()
+
+
 class AtBats(list):
-    
+
     def save(self):
         DB = store.Store()
         for inning in self:
             for atbat in inning:
-                keys = [k for k in atbat.keys() if k != 'pitches']
+                keys = [k for k in atbat.keys() if k not in ('pitches',
+                                                             'runners')]
                 values = [None if atbat[k] == '' else atbat[k] for k in keys]
-                
+
                 sql ='REPLACE INTO atbat (%s) VALUES(%s)' % (','.join(keys), ','.join(['%s'] * len(keys)))
                 DB.query(sql, values)
                 DB.save()
-                
+
                 for pitch in atbat['pitches']:
                     pitch.save()
-    
+
+                for runner in atbat['runners']:
+                    runner.save()
+
     def __init__(self, gid, game_id):
         super(AtBats,self).__init__()
 
         year, month, day = gid.split('_')[1:4]
         url = '%syear_%s/month_%s/day_%s/%s/inning/' % (CONSTANTS.BASE, year, month, day, gid)
-        
+
         contents = Fetcher.fetch(url)
         if contents is None:
             return
-        
+
         soup = BeautifulSoup(contents)
 
         inning_num = 1
@@ -68,9 +85,9 @@ class AtBats(list):
             if search(r'inning_\d+\.xml', inning_link['href']):
                 inning_url = '%s%s' % (url, inning_link['href'])
                 doc = minidom.parseString(Fetcher.fetch(inning_url))
-                
+
                 inning = []
-                
+
                 for atbat in doc.getElementsByTagName('atbat'):
                     values = {}
                     half = atbat.parentNode.nodeName
@@ -81,7 +98,8 @@ class AtBats(list):
                     values['game_id'] = game_id
                     values['inning'] = inning_num
                     values['pitches'] = []
-                    
+                    values['runners'] = []
+
                     balls = 0
                     strikes = 0
                     for pitch in atbat.getElementsByTagName('pitch'):
@@ -98,7 +116,27 @@ class AtBats(list):
                         elif pitch.attributes['type'].value == 'S':
                             strikes = strikes + 1
 
+                    for runner in atbat.getElementsByTagName('runner'):
+                        runner_attr = {
+                            'game_id': game_id,
+                            'atbat': atbat.attributes['num'].value,
+                            'runner': runner.attributes['id'].value,
+                            'start_base': runner.attributes['start'].value,
+                            'end_base': runner.attributes['end'].value,
+                        }
+
+                        for attr in ('score', 'rbi', 'earned'):
+                            try:
+                                if runner.attributes[attr].value == 'T':
+                                    runner_attr[attr] = 1
+                            except:
+                                pass
+
+                        r = Runner(runner, **runner_attr)
+                        values['runners'].append(r)
+
+
                     inning.append(values)
+
                 self.append(inning)
                 inning_num += 1
-
